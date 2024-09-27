ba95a4b3aaf85639f4d52f65135d4a89e4e670c8
Бекзод <90417312+SBek95@users.noreply.github.com>
2024-09-27 16:08:54 +0300

    first commit

---
+++ src/main/java/com/example/thynglieaf/ThynglieafApplication.java
@@ -0,0 +1,13 @@
+package com.example.thynglieaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThynglieafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThynglieafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thynglieaf/ThynglieafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thynglieaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThynglieafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
